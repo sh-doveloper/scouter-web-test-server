e21d6e7171c09226eb4711f170e8c9114e026cf4
sh-doveloper <sh.doveloper@gmail.com>
2025-03-10 00:14:45 +0900

    feat: merge chart 테스트 api 생성

--- src/main/java/com/scouter/scouterwebtestserver/controller/WebTestController.java
+++ src/main/java/com/scouter/scouterwebtestserver/controller/WebTestController.java
@@ -8,16 +8,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@CrossOrigin(origins = "http://localhost:3000") // React URL 허용
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class WebTestController {
     private final FileReaderService fileReaderService;
 
-    @GetMapping("/data")
-    public ResponseEntity<String> getData() {
+    @GetMapping("/chart-data")
+    public ResponseEntity<String> getGraphData() {
         try {
-            String jsonData = fileReaderService.readJsonFile("sample.json");
+            String jsonData = fileReaderService.readJsonFile("chart-data.json");
             return ResponseEntity.ok(jsonData); // HTTP 200 응답
         } catch (IOException e) {
             return ResponseEntity.status(500).body("{\"error\": \"Failed to load data\"}");
